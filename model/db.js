cf783116212b106f44f331b8279f12b421ea27ab
Your Name <you@example.com>
2018-07-06 14:31:38 +0530

    make it better

--- model/db.js
+++ model/db.js
@@ -10,7 +10,7 @@ const db = new Sequelize('mytestdb1', '','',{
 		max:5,
 		idle:5000,
 	 },
-	 "storage":"./mytestdb.sqlite"
+	 "storage":"./mybookdb.sqlite"
 });
 //user table
 const User = db.define('users',{
